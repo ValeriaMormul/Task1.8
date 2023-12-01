9793646d2dbd725385a11cda5729b70c951bb830
Valeriia Mormul <152411242+ValeriaMormul@users.noreply.github.com>
2023-12-02 01:49:55 +0200

    Добавьте файлы проекта.

---
+++ Task1.8/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        Console.WriteLine("2"); //Введенняя значення n
+        int n = int.Parse(Console.ReadLine());
+        int sum = CalculateSumOfFirstNNumbers(n);
+        Console.WriteLine($"Сума перших {n} натуральних чисел: {sum}");
+    }
+    static int CalculateSumOfFirstNNumbers(int n)
+    {
+        return n * (n + 1) / 2;
+    }
+}
